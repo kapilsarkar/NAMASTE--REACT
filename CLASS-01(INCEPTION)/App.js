f4dd53179faababca628d8dec815b1e830529b29
kapilsarkar <k.sarkar81@gmail.com>
2024-07-10 07:32:38 +0530

    updated

--- CLASS-01(INCEPTION)/App.js
+++
@@ -1,8 +0,0 @@
-const heading = React.createElement(
-    "h1",
-    {id:"heading"},
-    "Hello World From React"
-    );
-    console.log(heading)
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
--- CLASS-01(INCEPTION)/README.md
+++
@@ -1,1 +0,0 @@
-# NAMSTE REACT
