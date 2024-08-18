32d2f7c40556d54f4dcb49481317021df9937ffb
kapilsarkar <k.sarkar81@gmail.com>
2024-08-18 13:10:38 +0530

    somepartsadded

---
+++ EPISODE-02-IGNITING OUR APP/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import ReactDOM from "react-dom/client"
+
+const parent = React.createElement("div", { id: "parent" }, [
+    React.createElement("div", { id: "child" }, [
+      React.createElement("h1", {}, "I am an h1 tag created by Kapil"),
+      React.createElement("h2", {}, "Hi i am h2 tag"),
+    ]),
+    React.createElement("div", { id: "child2" }, [
+      React.createElement("h1", {}, "I am an h1 tag created by Piku"),
+      React.createElement("h2", {}, "Hi i am h2 tag"),
+    ]),
+  ]);
+  //console.log(parent);
+  
+  const heading = React.createElement(
+    "h1",
+    { id: "heading" },
+    "Hello World From React by Kapil Sarkar"
+  );
+  console.log(heading); //this will return an object
+  const root = ReactDOM.createRoot(document.getElementById("root"));
+  
+  root.render(heading);
+  root.render(parent);
