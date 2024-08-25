466a16ed3f309430cce68ce8d76e2a3cea2f2d65
kapilsarkar <k.sarkar81@gmail.com>
2024-08-25 20:20:41 +0530

    somepartsadded

--- EPISODE-03-LAYING THE FONDATION/App.js
+++ EPISODE-03-LAYING THE FONDATION/App.js
@@ -1,13 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-//React.createElement =>ReactElement -JS Object => HTMLElement(render)
+//React Element =>
 
-//JSX 
+const heading = <h1 className="head">Namaste React using JSX 🚀</h1>;
+console.log(heading);
 
-const jsxHeading = <h1 className="head">Namaste React using JSX  🚀</h1>
-console.log(jsxHeading)
+//React Functional Component
+
+const HeadingComponent = () => (
+  <h1 className="heading">Namaste React Functional Component-1</h1>
+);
+
+const HeadingComponent2 = ()=>{
+    return <h1 className="heading2">Namaste React Functional Component-2</h1>
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(jsxHeading);
+root.render(heading);
