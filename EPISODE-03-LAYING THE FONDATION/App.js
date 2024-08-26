3613199c6d1ca936e63c0f535a48b0f31199f3fe
kapilsarkar <k.sarkar81@gmail.com>
2024-08-26 07:29:20 +0530

    somepartsadded

--- EPISODE-03-LAYING THE FONDATION/App.js
+++ EPISODE-03-LAYING THE FONDATION/App.js
@@ -1,21 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+const elem = <span>React New Element </span>
+
 //React Element =>
 
-const heading = <h1 className="head">Namaste React using JSX 🚀</h1>;
+const heading = (<h1 className="head">{elem},React Element : Namaste React using JSX 🚀</h1>
+
+);
 console.log(heading);
 
+const Title = ()=>{
+  return <h1 className="head">Title Component : Namaste React using JSX 🚀</h1>
+}
+const number = 1000
 //React Functional Component
 
 const HeadingComponent = () => (
-  <h1 className="heading">Namaste React Functional Component-1</h1>
+  <div id="container">
+    <Title/>
+    <h2>{number}</h2>
+    <h3>{heading}</h3>
+    <h1 className="heading">Namaste React Functional Component-1</h1>
+  </div>
 );
 
-const HeadingComponent2 = ()=>{
-    return <h1 className="heading2">Namaste React Functional Component-2</h1>
-}
+const HeadingComponent2 = () => {
+  return <h1 className="heading2">Namaste React Functional Component-2</h1>;
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+root.render(<HeadingComponent/>);
