e1243f59e3c34d3073bf52006c4c5c31cbd46820
kapilsarkar <k.sarkar81@gmail.com>
2024-10-19 17:52:08 +0530

    somepartsadded

--- EPISODE-07-FINDING THE PATH/src/components/RestaurantMenu.js
+++ EPISODE-07-FINDING THE PATH/src/components/RestaurantMenu.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState, useEffect } from "react";
 import Shimmer from "./Shimmer";
 
 const RestaurantMenu = () => {
@@ -9,24 +9,26 @@ const RestaurantMenu = () => {
 
   const fetchMenu = async () => {
     const data = await fetch(
-      "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=22.5743545&lng=88.3628734&restaurantId=25142&catalog_qa=undefined&submitAction=ENTER"
+      "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=22.5743545&lng=88.3628734&restaurantId=809673&catalog_qa=undefined&submitAction=ENTER"
     );
     const json = await data.json();
     console.log(json);
-    setResInfo(json?.data);
+    setResInfo(json.data);
   };
 
-  const { name, cuisines, costForTwoMessage, city} =
-    resInfo?.cards[2]?.card?.card?.info;
-
+  
+  const {name,cuisines} = resInfo?.cards[2]?.card?.card?.info || {};
   return resInfo === null ? (
     <Shimmer />
   ) : (
     <div className="menu">
       <h1>{name}</h1>
-      <p>{cuisines}</p>
-      <p>{costForTwoMessage}</p>
-      <h2>{city}</h2>
+      <p>{cuisines.join(', ')}</p>
+      <p>{resInfo?.cards[2]?.card?.card?.info?.city}</p>
+      <p>{resInfo?.cards[2]?.card?.card?.info?.costForTwoMessage}</p>
+      <p>{resInfo?.cards[2]?.card?.card?.info?.locality}</p>
+      <p>{resInfo?.cards[2]?.card?.card?.info?.areaName}</p>
+      <p>{resInfo?.cards[2]?.card?.card?.info?.avgRating}</p>
       <h3>Menu</h3>
       <ul>
         <li>Biryani</li>
