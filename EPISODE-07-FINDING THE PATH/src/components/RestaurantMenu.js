154de533c186fbb6c291f844f749f4a415b135ca
kapilsarkar <k.sarkar81@gmail.com>
2024-10-20 08:35:45 +0530

    somepartsadded

--- EPISODE-07-FINDING THE PATH/src/components/RestaurantMenu.js
+++ EPISODE-07-FINDING THE PATH/src/components/RestaurantMenu.js
@@ -1,39 +1,55 @@
 import { useState, useEffect } from "react";
 import Shimmer from "./Shimmer";
+import { useParams } from "react-router-dom";
 
 const RestaurantMenu = () => {
   const [resInfo, setResInfo] = useState(null);
+  const {resId} = useParams();
+ 
   useEffect(() => {
     fetchMenu();
   }, []);
 
   const fetchMenu = async () => {
     const data = await fetch(
-      "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=22.5743545&lng=88.3628734&restaurantId=809673&catalog_qa=undefined&submitAction=ENTER"
+      "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=22.5743545&lng=88.3628734&restaurantId=816194&catalog_qa=undefined&submitAction=ENTER"
     );
     const json = await data.json();
     console.log(json);
     setResInfo(json.data);
   };
 
-  
-  const {name,cuisines} = resInfo?.cards[2]?.card?.card?.info || {};
-  return resInfo === null ? (
-    <Shimmer />
-  ) : (
+  if (resInfo === null) return <Shimmer />;
+
+  const {
+    name,
+    cuisines,
+    city,
+    costForTwoMessage,
+    locality,
+    areaName,
+    avgRating,
+  } = resInfo?.cards[2]?.card?.card?.info;
+
+  const { itemCards } =
+    resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card?.card;
+  console.log(itemCards);
+  return (
     <div className="menu">
       <h1>{name}</h1>
-      <p>{cuisines.join(', ')}</p>
-      <p>{resInfo?.cards[2]?.card?.card?.info?.city}</p>
-      <p>{resInfo?.cards[2]?.card?.card?.info?.costForTwoMessage}</p>
-      <p>{resInfo?.cards[2]?.card?.card?.info?.locality}</p>
-      <p>{resInfo?.cards[2]?.card?.card?.info?.areaName}</p>
-      <p>{resInfo?.cards[2]?.card?.card?.info?.avgRating}</p>
+      <p>{cuisines.join(", ")}</p>
+      <p>{city}</p>
+      <p>{costForTwoMessage}</p>
+      <p>{locality}</p>
+      <p>{areaName}</p>
+      <p>{avgRating}</p>
       <h3>Menu</h3>
       <ul>
-        <li>Biryani</li>
-        <li>Burgers</li>
-        <li>Diet Coke</li>
+        {itemCards.map((item)=> {
+          return(
+            <li key={item.card.info.id}>{item.card.info.name} - {"Rs"}{item.card.info.price/100}</li>
+          )
+        })}
       </ul>
     </div>
   );
