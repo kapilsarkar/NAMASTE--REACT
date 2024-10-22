14d128b4bde449bf880631905858a164fa6bc4e2
kapilsarkar <k.sarkar81@gmail.com>
2024-10-22 13:50:29 +0530

    somepartsadded

--- EPISODE-07-ASSIGNMENT/src/components/Body.js
+++ EPISODE-07-ASSIGNMENT/src/components/Body.js
@@ -1,5 +1,6 @@
 import { RESTAURANT_URL } from "../utils/constant";
 import TopRestaurant from "./TopRestaurant";
+import Shimmer from "./Shimmer";
 import { useEffect, useState } from "react";
 const Body = () => {
   const [topRestaurant, setTopRestaurant] = useState([]);
@@ -16,32 +17,48 @@ const Body = () => {
       json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
     );
   };
-  
-  const handleSearch = ()=>{
-    
-  }
-  return (
-    <div className="Body">
-      <div className="search">
-        <input
-          type="text"
-          className="search-box"
-          placeholder="Search..."
-          value={searchText}
-          onChange={(e) => setSearchText(e.target.value)}
-        />
-        <button className="search-btn">Search</button>
-      </div>
 
-      <h2 className="TopHeading" id="TopHeading">
-        Top Restaurants
-      </h2>
-      <div className="top-res">
-        {topRestaurant.map((restaurant) => {
-          return (
-            <TopRestaurant key={restaurant.info.id} resData={restaurant} />
-          );
-        })}
+  const handleSearch = () => {
+    const filterData = topRestaurant.filter((res) => {
+      return res.info.name.toLowerCase().includes(searchText.toLowerCase());
+    });
+    setTopRestaurant(filterData);
+  };
+  const allSearch = () => {
+    fetchTopRestaurant();
+    setSearchText("");
+  };
+  return topRestaurant.length === 0 ? (
+    <Shimmer />
+  ) : (
+    <div className="Body">
+      <div className="container">
+        <div className="search">
+          <input
+            type="text"
+            className="search-box"
+            placeholder="Search..."
+            value={searchText}
+            onChange={(e) => setSearchText(e.target.value)}
+          />
+          <button className="search-btn" onClick={handleSearch}>
+            Search
+          </button>
+          <button className="allRes-btn" onClick={allSearch}>
+            All Restaurants
+          </button>
+        </div>
+
+        <h2 className="TopHeading" id="TopHeading">
+          Top Restaurants
+        </h2>
+        <div className="top-res">
+          {topRestaurant.map((restaurant) => {
+            return (
+              <TopRestaurant key={restaurant.info.id} resData={restaurant} />
+            );
+          })}
+        </div>
       </div>
     </div>
   );
