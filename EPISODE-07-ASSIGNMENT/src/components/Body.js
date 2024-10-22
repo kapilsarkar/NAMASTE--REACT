0142fe938cd1bb9397d92f59001689779d4f9aef
kapilsarkar <k.sarkar81@gmail.com>
2024-10-22 13:07:27 +0530

    somepartsadded

--- EPISODE-07-ASSIGNMENT/src/components/Body.js
+++ EPISODE-07-ASSIGNMENT/src/components/Body.js
@@ -1,8 +1,9 @@
 import { RESTAURANT_URL } from "../utils/constant";
 import TopRestaurant from "./TopRestaurant";
-import { useEffect,useState } from "react";
+import { useEffect, useState } from "react";
 const Body = () => {
-  const [topRestaurant,setTopRestaurant] = useState([]);
+  const [topRestaurant, setTopRestaurant] = useState([]);
+  const [searchText, setSearchText] = useState("");
   useEffect(() => {
     fetchTopRestaurant();
   }, []);
@@ -11,22 +12,36 @@ const Body = () => {
     const data = await fetch(RESTAURANT_URL);
     const json = await data.json();
     console.log(json);
-    setTopRestaurant(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
+    setTopRestaurant(
+      json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
   };
-
+  
+  const handleSearch = ()=>{
+    
+  }
   return (
     <div className="Body">
+      <div className="search">
+        <input
+          type="text"
+          className="search-box"
+          placeholder="Search..."
+          value={searchText}
+          onChange={(e) => setSearchText(e.target.value)}
+        />
+        <button className="search-btn">Search</button>
+      </div>
+
       <h2 className="TopHeading" id="TopHeading">
         Top Restaurants
       </h2>
-      <div className="top-res">{
-        topRestaurant.map((restaurant)=>{
-          return(
-            <TopRestaurant key={restaurant.info.id} resData={restaurant}/>
-          )
-        })
-        }
-       
+      <div className="top-res">
+        {topRestaurant.map((restaurant) => {
+          return (
+            <TopRestaurant key={restaurant.info.id} resData={restaurant} />
+          );
+        })}
       </div>
     </div>
   );
