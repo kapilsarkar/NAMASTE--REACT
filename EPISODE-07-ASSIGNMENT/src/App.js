241f3c653dea6b8696ef6c18961e17e6753b6f07
kapilsarkar <k.sarkar81@gmail.com>
2024-10-23 07:21:46 +0530

    somepartsadded

--- EPISODE-07-ASSIGNMENT/src/App.js
+++ EPISODE-07-ASSIGNMENT/src/App.js
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
+import { createBrowserRouter,RouterProvider,Outlet } from "react-router-dom";
+import About from "./components/About";
+import Contact from "./components/Contact";
 const AppLayout = () => {
     return (
       <div className="app">
@@ -10,6 +13,15 @@ const AppLayout = () => {
       </div>
     );
   };
-
+const appRouter = createBrowserRouter([
+  {
+    path:"/",
+    element: <AppLayout/>,
+  },
+  {
+    path:"/about",
+    element: <About/>, 
+  },
+])
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<AppLayout />);
