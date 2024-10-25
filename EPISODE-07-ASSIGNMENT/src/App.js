55c8edaa5ff133a2d21ce08662b6e9a04dcabffd
kapilsarkar <k.sarkar81@gmail.com>
2024-10-25 08:47:48 +0530

    somepartsadded

--- EPISODE-07-ASSIGNMENT/src/App.js
+++ EPISODE-07-ASSIGNMENT/src/App.js
@@ -6,6 +6,7 @@ import { createBrowserRouter,RouterProvider,Outlet } from "react-router-dom";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import  Error  from "./components/Error";
+import RestaurantMenu from "./components/RestaurantMenu";
 const AppLayout = () => {
     return (
       <div className="app">
@@ -30,6 +31,10 @@ const appRouter = createBrowserRouter([
       {
         path:"/contact",
         element:<Contact/>,
+      },
+      {
+        path:"/restaurants/:resId",
+        element:<RestaurantMenu/>,
       },
     ],
     errorElement: <Error/>,
--- EPISODE-07-ASSIGNMENT/src/components/RestaurantMenu.js
+++ EPISODE-07-ASSIGNMENT/src/components/RestaurantMenu.js
@@ -1,7 +1,36 @@
-const RestaurantMenu = ()=>{
-    return(
-        <div>
+import { useEffect,useState } from "react";
+import Shimmer from "./Shimmer";
+import { MENU_API, MENU_IMG } from "../utils/constant";
+const RestaurantMenu = () => {
+  const [resInfo,setResInfo] = useState(null)
+  useEffect(() => {
+    fetchMenu();
+  }, []);
+  const fetchMenu = async () => {
+    const data = await fetch(MENU_API);
+    const json = await data.json();
+    console.log(json);
+    setResInfo(json.data)
+  };
+  if (resInfo === null) return <Shimmer />;
+  const {name, cuisines,
+    city,
+    costForTwoMessage,
+    avgRating,cloudinaryImageId} = resInfo?.cards[2]?.card?.card?.info;
+  return (
+    <div className="restaurant-menu">
+      <img src={MENU_IMG+cloudinaryImageId}/>
+      <h1>{name}</h1>
+      <p>{cuisines.join(",")}</p>
+      <p>{city}</p>
+      <p>{costForTwoMessage}</p>
+      <p>{avgRating}</p>
+      <ul>
+        <li>Biryani</li>
+        <li>Burger</li>
+      </ul>
+    </div>
+  );
+};
 
-        </div>
-    )
-}
+export default RestaurantMenu;
