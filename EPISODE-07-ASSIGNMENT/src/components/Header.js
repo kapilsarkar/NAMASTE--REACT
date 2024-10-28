474b475fe70770d504f77dfa84f6cd4f77f8df56
kapilsarkar <k.sarkar81@gmail.com>
2024-10-28 07:32:06 +0530

    somepartsadded

--- EPISODE-07-ASSIGNMENT/src/components/Header.js
+++ EPISODE-07-ASSIGNMENT/src/components/Header.js
@@ -2,37 +2,47 @@ import { LOGO_URL } from "../utils/constant";
 import { FaCartArrowDown } from "react-icons/fa";
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import City from "./City";
 const Header = () => {
   const [btnNameReact, setBtnNameReact] = useState("Login");
   return (
-    <div className="header">
-      <Link href="/" className="anchor-head">
-        <div className="logo-container">
-          <img className="logo" src={LOGO_URL} />
-          <h3 className="food-heading">Food App</h3>
-        </div>
-      </Link>
+    <div>
+      <div className="header">
+        <Link href="/" className="anchor-head">
+          <div className="logo-container">
+            <img className="logo" src={LOGO_URL} />
+            <h3 className="food-heading">Food App</h3>
+          </div>
+        </Link>
 
-      <div className="nav-items">
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">About Us</Link></li>
-          <li><Link to="/contact">Contact Us</Link></li>
-          <li>
-            <FaCartArrowDown />
-          </li>
-        </ul>
+        <div className="nav-items">
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About Us</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact Us</Link>
+            </li>
+            <li>
+              <FaCartArrowDown />
+            </li>
+          </ul>
+        </div>
+        <button
+          className="loginBtn"
+          onClick={() => {
+            btnNameReact === "Login"
+              ? setBtnNameReact("Logout")
+              : setBtnNameReact("Login");
+          }}
+        >
+          {btnNameReact}
+        </button>
       </div>
-      <button
-        className="loginBtn"
-        onClick={() => {
-          btnNameReact === "Login"
-            ? setBtnNameReact("Logout")
-            : setBtnNameReact("Login");
-        }}
-      >
-        {btnNameReact}
-      </button>
+      <City/>
     </div>
   );
 };
--- EPISODE-07-ASSIGNMENT/src/utils/constant.js
+++ EPISODE-07-ASSIGNMENT/src/utils/constant.js
@@ -20,8 +20,18 @@ export const ABOUT_IMG =
 export const CONTACT_IMG =
   "https://apptopia.com/blog/wp-content/uploads/2023/09/shutterstock_1291959472-copy-1.png";
 
-export const MENU_API = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=22.5743545&lng=88.3628734&restaurantId=";
+export const MENU_API =
+  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=22.5743545&lng=88.3628734&restaurantId=";
 
-export const MENU_IMG = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_300,h_300,c_fit/" ;
+export const MENU_IMG =
+  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_300,h_300,c_fit/";
 
-export const EACH_MENU_IMG = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_300,h_300,c_fit/"
+export const EACH_MENU_IMG =
+  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_300,h_300,c_fit/";
+
+
+  export const KOLKATA_IMG = "https://i0.wp.com/thestrongtraveller.com/wp-content/uploads/2021/04/00100dPORTRAIT_00100_BURST20200619171529597_COVER-02-min.jpg?fit=960%2C720&ssl=1";
+
+  export const DELHI_IMG = "https://moha-mushkil.com/wp-content/uploads/2018/08/Chache-di-hatti-.jpg";
+  
+  export const BANGALORE_IMG = "https://www.treebo.com/blog/wp-content/uploads/2018/03/Dosas.jpg"
