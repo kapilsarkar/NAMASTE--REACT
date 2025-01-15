ec03965c2603f1a394fdae08e6aca31f3aa23f2a
kapilsarkar <k.sarkar81@gmail.com>
2025-01-15 08:35:29 +0530

    somepartsadded

--- EPISODE-08-LET'S GET CLASSY/src/components/Header.js
+++ EPISODE-08-LET'S GET CLASSY/src/components/Header.js
@@ -1,30 +1,11 @@
 import { LOGO_URL } from "../utils/constant";
-import { useState } from "react";
+
 const Header = () => {
-  const [btnNameReact, setBtnNameReact] = useState("Login");
   return (
     <div className="header">
       <div className="logo-container">
         <img className="logo" src={LOGO_URL} />
         <h3 className="food-heading">Food App</h3>
-      </div>
-      <div className="nav-items">
-        <ul>
-          <li>Home</li>
-          <li>About Us</li>
-          <li>Contact Us</li>
-          <li>Cart</li>
-          <button
-            className="loginBtn"
-            onClick={() => {
-              btnNameReact === "Login"
-                ? setBtnNameReact("Logout")
-                : setBtnNameReact("Login");
-            }}
-          >
-            {btnNameReact}
-          </button>
-        </ul>
       </div>
     </div>
   );
--- EPISODE-08-LET'S GET CLASSY/src/utils/constant.js
+++ EPISODE-08-LET'S GET CLASSY/src/utils/constant.js
@@ -1,5 +1,5 @@
-export const CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+export const LOGO_URL =
+  "https://w7.pngwing.com/pngs/55/100/png-transparent-swiggy-hd-logo-thumbnail.png";
 
-export const LOGO_URL = "https://cdn-icons-png.freepik.com/256/958/958170.png?semt=ais_hybrid"
-
-export const SHIMMER_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/b0jfcdvyjkw2owk3dhud"
+export const SHIMMER_URL =
+  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/b0jfcdvyjkw2owk3dhud";
