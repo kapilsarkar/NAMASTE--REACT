0dbe766b6c9eaaf029a7887b47823321e16df051
kapilsarkar <k.sarkar81@gmail.com>
2025-01-18 08:32:17 +0530

    somepartsadded

--- EPISODE-08-LET'S GET CLASSY/src/App.js
+++ EPISODE-08-LET'S GET CLASSY/src/App.js
@@ -3,13 +3,40 @@ import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
 import Footer from "./components/Footer";
+import { createBrowserRouter, RouterProvider,Outlet } from "react-router-dom";
+import About from "./components/About";
+import Contact from "./components/Contact";
 const AppLayout = () => {
-  return <div className="app">
-    <Header/>
-    <Body/>
-    <Footer/>
-  </div>;
+  return (
+    <div className="app">
+      <Header />
+      <Outlet/>
+      <Footer />
+    </div>
+  );
 };
 
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    children: [
+      {
+          path:"/",
+          element: <Body/>,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+    ],
+    errorElement: <Error />,
+  },
+]);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<AppLayout />);
+root.render(<RouterProvider router={appRouter} />);
--- EPISODE-08-LET'S GET CLASSY/src/components/Header.js
+++ EPISODE-08-LET'S GET CLASSY/src/components/Header.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { LOGO_URL } from "../utils/constant";
-
+import { Link } from "react-router-dom";
 const Header = () => {
   const [btnName, setBtnName] = useState("Login");
   return (
@@ -11,9 +11,15 @@ const Header = () => {
       </div>
       <div className="nav-items">
         <ul>
-          <li>Home</li>
-          <li>About Us</li>
-          <li>Contact Us</li>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About Us</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact Us</Link>{" "}
+          </li>
           <li>Cart</li>
           <button
             className="loginBtn"
