41459fc4ca7a15d2bea549da54faf12fc6c68f9a
kapilsarkar <k.sarkar81@gmail.com>
2025-01-19 08:42:48 +0530

    somepartsadded

--- EPISODE-08-LET'S GET CLASSY/src/components/RestaurantMenu.js
+++ EPISODE-08-LET'S GET CLASSY/src/components/RestaurantMenu.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import Shimmer from "./Shimmer";
-
+import { MdStarRate } from "react-icons/md";
+import { EACH_MENU_IMG } from "../utils/constant";
 const RestaurantMenu = () => {
   const [resInfo, setResInfo] = useState(null);
 
@@ -14,15 +15,46 @@ const RestaurantMenu = () => {
     );
     const json = await data.json();
     console.log(json);
+    setResInfo(json.data);
   };
-  
+  if (resInfo === null) return <Shimmer />;
+  const {
+    name,
+    cuisines,
+    city,
+    costForTwoMessage,
+    avgRating,
+    cloudinaryImageId,
+  } = resInfo?.cards[2]?.card?.card?.info;
+
+  const { itemCards } =
+    resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card?.card;
   return (
     <div className="restaurant-menu">
-      <h2>Name Of Restaurant</h2>
-      <h2>Menu</h2>
+      <div className="restaurant-summary">
+        <div className="restaurant-summary-details">
+          <h2 className="restaurant-title">{name}</h2>
+          <p>{cuisines.join(",")}</p>
+          <p>{city}</p>
+          <p>{costForTwoMessage}</p>
+        </div>
+      </div>
+      <h3 className="recommend-head">Menu</h3>
       <ul>
-        <li>Biryani</li>
-        <li>Burger</li>
+        {itemCards.map((item) => {
+          return (
+            <div key={item.card.info.id} className="all-menu">
+              <div className="all-menu-description-left">
+                <h3>{item?.card?.info?.name}</h3> - {"₹"}
+                {item?.card?.info?.price}
+                <p className="menu-desc">{item?.card?.info?.description}</p>
+              </div>
+              <div className="all-menu-description-right">
+                <img src={EACH_MENU_IMG + item?.card?.info?.imageId} />
+              </div>
+            </div>
+          );
+        })}
       </ul>
     </div>
   );
--- EPISODE-08-LET'S GET CLASSY/src/utils/constant.js
+++ EPISODE-08-LET'S GET CLASSY/src/utils/constant.js
@@ -12,3 +12,6 @@ export const SHIMMER_URL =
 
   export const CONTACT_IMG =
   "https://apptopia.com/blog/wp-content/uploads/2023/09/shutterstock_1291959472-copy-1.png";
+
+  export const EACH_MENU_IMG =
+  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_300,h_300,c_fit/";
