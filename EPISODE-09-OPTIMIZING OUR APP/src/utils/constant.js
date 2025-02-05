78c7f9e74f795f9fbcca16dc6edf15bfe7988a6b
kapilsarkar <k.sarkar81@gmail.com>
2025-02-05 07:53:18 +0530

    somepartsadded

---
+++ EPISODE-09-OPTIMIZING OUR APP/src/utils/constant.js
@@ -0,0 +1,23 @@
+export const CDN_URL =
+  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+
+export const LOGO_URL =
+  "https://w7.pngwing.com/pngs/55/100/png-transparent-swiggy-hd-logo-thumbnail.png";
+
+export const SHIMMER_URL =
+  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/b0jfcdvyjkw2owk3dhud";
+
+export const ABOUT_IMG =
+  "https://blog.swiggy.com/wp-content/uploads/2024/07/1000075667-1000x486.png";
+
+export const CONTACT_IMG =
+  "https://apptopia.com/blog/wp-content/uploads/2023/09/shutterstock_1291959472-copy-1.png";
+
+export const MENU_API =
+  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=22.5743545&lng=88.3628734&restaurantId=";
+
+export const EACH_MENU_IMG =
+  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_300,h_300,c_fit/";
+
+export const MENU_IMG =
+  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_300,h_300,c_fit/";
