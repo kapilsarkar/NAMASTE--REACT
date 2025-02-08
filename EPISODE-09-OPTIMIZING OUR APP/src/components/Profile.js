090115709317a47ff4af603e67a3557f1596f2c0
kapilsarkar <k.sarkar81@gmail.com>
2025-02-08 17:17:58 +0530

    somepartsadded

--- EPISODE-09-OPTIMIZING OUR APP/src/components/Profile.js
+++ EPISODE-09-OPTIMIZING OUR APP/src/components/Profile.js
@@ -1,7 +1,7 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const Profile = ()=>{
-
+ const [profileData,setProfileData] = useState("");
   useEffect(()=>{
        fetchProfile();
   },[])
@@ -9,11 +9,13 @@ const Profile = ()=>{
       const data = await fetch("https://api.github.com/users/kapilsarkar");
       const json = await  data.json();
       console.log(json);
+      setProfileData(json)
   }
     return (
         <div className="">
-          <h2>Kapil Sarkar</h2>
-          fasfas
+          <h2>{profileData.name}</h2>
+          <img src={profileData.avatar_url}/>
+          <p>{profileData.bio}</p>
         </div>
     )
 }
