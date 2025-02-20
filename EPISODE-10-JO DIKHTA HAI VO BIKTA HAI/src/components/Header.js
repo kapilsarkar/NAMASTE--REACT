0730c8013d3e5ff17cbafeffd7f9a9e19dda9177
kapilsarkar <k.sarkar81@gmail.com>
2025-02-20 12:35:29 +0530

    somepartsadded

--- EPISODE-10-JO DIKHTA HAI VO BIKTA HAI/src/components/Header.js
+++ EPISODE-10-JO DIKHTA HAI VO BIKTA HAI/src/components/Header.js
@@ -13,22 +13,26 @@ const Header = () => {
       </div>
       <div className=" flex justify-center font-bold">
         <ul className=" flex gap-2 justify-between p-2 cursor-pointer">
-          <li className="p-1.5">Online Status {onlineStatus ? "🟢" : "🔴"}</li>
-          <li className="p-1.5">
+          <li className="p-1.5 animate-pulse">
+            Online Status {onlineStatus ? "🟢" : "🔴"}
+          </li>
+          <li className="p-1.5 rounded-xs hover:duration-300 hover:ease-in-out hover:bg-orange-500 hover:text-white">
             <Link to="/">Home</Link>
           </li>
-          <li className="p-1.5 hover:duration-300 hover:ease-in-out hover:bg-orange-500 hover:text-white">
+          <li className="p-1.5 rounded-xs hover:duration-300 hover:ease-in-out hover:bg-orange-500 hover:text-white">
             <Link to="/about">About Us</Link>
           </li>
-          <li className="p-1.5">
+          <li className="p-1.5 rounded-xs hover:duration-300 hover:ease-in-out hover:bg-orange-500 hover:text-white">
             <Link to="/contact">Contact Us</Link>{" "}
           </li>
-          <li className="p-1.5">
+          <li className="p-1.5 rounded-xs hover:duration-300 hover:ease-in-out hover:bg-orange-500 hover:text-white">
             <Link to="/grocery">Grocery</Link>
           </li>
-          <li className="p-1.5">Cart</li>
+          <li className="p-1.5 rounded-xs hover:duration-300 hover:ease-in-out hover:bg-orange-500 hover:text-white">
+            Cart
+          </li>
           <button
-            className="loginBtn"
+            className=" cursor-pointer bg-orange-600 text-white px-1.5 py-1 rounded-b-xl rounded-t-sm"
             onClick={() => {
               btnName === "Login" ? setBtnName("Logout") : setBtnName("Login");
             }}
