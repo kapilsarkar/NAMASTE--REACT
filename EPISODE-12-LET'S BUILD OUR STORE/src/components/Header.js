ae2bd2fdd1c659c1782d18ee2c606b6275c205d0
kapilsarkar <k.sarkar81@gmail.com>
2025-04-11 08:11:43 +0530

    somepartsadded

---
+++ EPISODE-12-LET'S BUILD OUR STORE/src/components/Header.js
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <div>
+      <h2 className=" text-2xl bg-amber-500 text-white">Food App</h2>
+    </div>
+  );
+};
+
+export default Header;
---
+++ EPISODE-12-LET'S BUILD OUR STORE/src/utils/constant.js
@@ -0,0 +1,5 @@
+export const LOGO_URL =
+  "https://w7.pngwing.com/pngs/55/100/png-transparent-swiggy-hd-logo-thumbnail.png";
+
+export const RESTAURANT_URL =
+  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.5355161&lng=77.3910265&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
