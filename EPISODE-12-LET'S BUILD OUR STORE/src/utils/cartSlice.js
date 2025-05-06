c6b307c8fbf10b4e4e2407b2b36ebb3f590e12ea
kapilsarkar <k.sarkar81@gmail.com>
2025-05-06 13:58:58 +0530

    somepartsadded

--- EPISODE-12-LET'S BUILD OUR STORE/src/utils/cartSlice.js
+++ EPISODE-12-LET'S BUILD OUR STORE/src/utils/cartSlice.js
@@ -7,14 +7,19 @@ const cartSlice = createSlice({
     },
     reducers: {
         addItem: (state, action) => {
+            //Redux ToolKit
             //Mutating the State here
+            //Redux ToolKit uses immer behind the state
             state.items.push(action.payload);
         },
         removeItem: (state) => {
             state.items.pop();
         },
         clearCart: (state) => {
-            state.items.length = 0; // []
+            //Redux Tool Kit - Either Mutate the existing state or return the new state
+            // state.items.length = 0; // originalState = []
+
+            return { items: [] }  // this new object will be replaced inside originalState = { items: [] }
         },
     },
 });
