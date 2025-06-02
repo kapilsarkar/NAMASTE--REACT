68d6b61f45207b34b22a87c2fe6efae14d0ebecb
kapilsarkar <k.sarkar81@gmail.com>
2025-06-02 07:18:50 +0530

    somepartsadded

---
+++ YOUTUBE-CLONE/README.md
@@ -0,0 +1,1 @@
+### YOUTUBE CLONE 
---
+++ YOUTUBE-CLONE/src/App.jsx
@@ -0,0 +1,12 @@
+
+function App() {
+  
+
+  return (
+    <>
+      <h2 className="text-2xl bg-red-500 text-white">YOUTUBE CLONE</h2>
+    </>
+  )
+}
+
+export default App
