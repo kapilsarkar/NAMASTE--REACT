660e1285b8871c355a0d490cb1d40c11e71a321b
kapilsarkar <k.sarkar81@gmail.com>
2025-06-08 07:06:56 +0530

    updated

--- EPISODE-13-TIME FOR TEST/src/utils/constant.js
+++ EPISODE-13-TIME FOR TEST/src/utils/constant.js
@@ -5,7 +5,7 @@ export const LOGO_URL =
   "https://w7.pngwing.com/pngs/55/100/png-transparent-swiggy-hd-logo-thumbnail.png";
 
 export const RESTAURANT_URL =
-  "https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.4954988&lng=88.3709008&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.4954988&lng=88.3709008&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
 
 export const SHIMMER_URL =
   "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/b0jfcdvyjkw2owk3dhud";
@@ -14,14 +14,14 @@ export const ABOUT_IMG =
   "https://blog.swiggy.com/wp-content/uploads/2024/07/1000075667-1000x486.png";
 
 export const CONTACT_IMG =
-  "https://apptopia.com/blog/wp-content/uploads/2023/09/shutterstock_1291959472-copy-1.png";
+  "https://www.shutterstock.com/image-illustration/contact-us-text-written-over-600nw-1080340454.jpg";
 
-export const MENU_API = "https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=22.5743545&lng=88.3628734&restaurantId=";
+export const MENU_API = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=22.5743545&lng=88.3628734&restaurantId=";
 
 export const EACH_MENU_IMG = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_300,h_300,c_fit/";
 
 export const MENU_IMG = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_300,h_300,c_fit/";
 
-export const MENU_API_ONLINE = "https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=22.4954988&lng=88.3709008&restaurantId=";
+export const MENU_API_ONLINE = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=22.4954988&lng=88.3709008&restaurantId=";
 
 export const MENU_IMG_ONLINE = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_292,h_300/";
