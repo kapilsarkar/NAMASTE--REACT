f156a997aadf86892854f2be1b84a6a1f0a7b82c
kapilsarkar <k.sarkar81@gmail.com>
2025-06-21 15:35:08 +0530

    somepartsadded

--- YOUTUBE-CLONE/src/components/Demo.jsx
+++ YOUTUBE-CLONE/src/components/Demo.jsx
@@ -1,8 +1,19 @@
+import { useState } from "react";
+
 const Demo = () => {
+  const [text, setText] = useState("");
+  console.log("Rendering");
   return (
     <>
       <div className="m-3 p-3 w-96 h-96 border border-black">
-        <h2>Demo</h2>
+        <div>
+          <input
+            className=" border border-black w-72 px-2"
+            type="text"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          />
+        </div>
       </div>
     </>
   );
